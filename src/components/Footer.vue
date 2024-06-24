c4c607ec8e509d57a3581dd521f3598c7b8f68d8
GopiAjt <gopiajt23@gmail.com>
2024-06-24 09:36:38 +0530

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer>
+        <p>Footer content goes here</p>
+    </footer>
+</template>
+
+<script setup>
+// If you're using Vue 3 with the `<script setup>` syntax
+// This is where you can add your component logic
+</script>
+
+<style scoped>
+/* Add scoped styles specific to Footer.vue */
+footer {
+    background-color: #f0f0f0;
+    padding: 10px;
+    text-align: center;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="navbar">
+        <a href="#"><img src="/src/assets/images/CaptureNow.png" alt="" width="45vw"></a>
+        <div>
+            <input type="" name="" id="">
+        </div>
+        <div>
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#navbar {
+    margin-top: 0%;
+    display: flex;
+    align-items: center;
+    width: 1500px;
+    height: 50px;
+    margin-top: 10px;
+    background-color: yellow;
+    justify-content: space-around;
+    border-radius: 5px;
+}
+</style>
